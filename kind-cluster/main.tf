dfaf0a58daed27ac4cbd47e941ca5840902c2121
jin0118 <lsj6445z@naver.com>
2025-05-17 01:04:40 +0900

    kind-cluser nodeport 노출 설정

--- kind-cluster/main.tf
+++ kind-cluster/main.tf
@@ -1,6 +1,21 @@
 module "cluster" {
   source  = "PePoDev/cluster/kind"
   version = "0.2.7"
-  # insert the 1 required variable here
+
   cluster_name = "kind-cluster"
+  
+  nodes = [
+    {
+      description = "kind cluster에서 Nodeport 오픈하기 위한 설정: https://kind.sigs.k8s.io/docs/user/configuration/"
+      role = "rancher service NodePort"
+      extra_port_mappings = {
+        container_port = "30980"
+        host_port      = "80"
+        listen_address = "0.0.0.0"
+        protocol       = "TCP"
+      }
+      kubeadm_config_patches = []
+    }
+  ]
 }
+
